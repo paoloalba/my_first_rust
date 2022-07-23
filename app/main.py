6a4a78938f7b61ba58379dc33eb6e3d46cb6c490
Paolo Alba <paoloalbag@gmail.com>
2022-07-23 22:45:05 +0200

    add submodule

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ print(type(UsState))
 print(UsState.Alaska)
 print(type(UsState.Alaska))
 
-import os
-from shutil import copytree
-
-copytree(os.path.join("/rust_inst"), os.path.join(os.getenv("PMT_STG_PATH"), "copied_rust_build"), dirs_exist_ok=True)
+# import os
+# from shutil import copytree
+# copytree(os.path.join("/rust_inst"), os.path.join(os.getenv("PMT_STG_PATH"), "copied_rust_build"), dirs_exist_ok=True)
 
 print("main is done")
--- docker_helpers/src/lib.rs
+++ docker_helpers/src/lib.rs
@@ -1,4 +1,14 @@
 #![allow(unused)]
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+
+
 fn main() {
 use pyo3::prelude::*;
 use rand::Rng;
